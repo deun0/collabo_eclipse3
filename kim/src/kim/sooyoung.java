ed251112d699e9afc478c8a0df9ad9a24c1850f5
young <young@naver.com>
2024-01-23 15:24:04 +0900

    안녕 나는 수영을 잘하는 수영이야

---
+++ kim/src/kim/sooyoung.java
@@ -0,0 +1,8 @@
+package kim;
+
+public class sooyoung {
+	public static void main(String[] args) {
+		System.out.println("나는 수영ㅇ이이잉");
+	}
+
+}
---
+++ kim/src/kim/yoonyoung.java
@@ -0,0 +1,8 @@
+package kim;
+
+public class yoonyoung {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요.");
+	}
+
+}
